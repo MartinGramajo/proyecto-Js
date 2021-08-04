6979fa83116bb6a2b7af086b71105a2a507a817d
magus <martingramajo08@gmail.com>
2021-08-04 00:57:26 -0300

    agrego registro de usuario

---
+++ registro.js
@@ -0,0 +1,5 @@
+const registroUsuario = document.getElementById('formularioRegistroAlta');
+const inputNombreUsuario = document.getElementById('inputNombreRegistro'); 
+const inputEmailUsuario = document.getElementById('inputEmailRegistro'); 
+const inputContraseñaUsuario = document.getElementById('inputContraRegistro');
+const inputConfirmarContraseña = document.getElementById('inputConfirmarContraseña'); 
